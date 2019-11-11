fcd4a4096321f0891503906ea08b1b2fb33be455
Guilherme Conti <guibasconti@gmail.com>
2019-11-11 13:55:54 -0300

    Cracking the code interview Stacks and Queues - 3-2

---
+++ crackingthecodeinterview/stacksandqueues/3-2.py
@@ -0,0 +1,42 @@
+# Design a stack that also contains a min function with O(1)
+
+# Solution 1 Create two stacks. One that holds every value and another that holds the min
+# If a value is popped check if it is the top of your stack, if so pop as well
+def insert(stack, minStack, value):
+  stack.append(value)
+  if len(minStack) == 0 or minStack[len(minStack) - 1] >= value:
+    minStack.append(value)
+
+def remove(stack, minStack):
+  if minStack[len(minStack) - 1] == stack.pop():
+    minStack.pop()
+
+def getMin(stack, minStack):
+  return minStack[len(minStack) - 1]
+
+
+# Solution 2 if no buffer is allowed we could for each element create a runner pointer
+# That will go through the remaining elements looking for duplicates O(n^2)
+
+if __name__ == '__main__':
+  stack = []
+  minStack = []
+  insert(stack, minStack, 10)
+  insert(stack, minStack, 13)
+  insert(stack, minStack, 8)
+  insert(stack, minStack, 5)
+  insert(stack, minStack, 8)
+  insert(stack, minStack, 5)
+  insert(stack, minStack, 13)
+  insert(stack, minStack, 11)
+  print(stack, minStack)
+  print(getMin(stack, minStack))
+  remove(stack, minStack)
+  remove(stack, minStack)
+  remove(stack, minStack)
+  print(stack, minStack)
+  remove(stack, minStack)
+  remove(stack, minStack)
+  print(getMin(stack, minStack))
+  remove(stack, minStack)
+  print(getMin(stack, minStack))
