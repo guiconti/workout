abca64578390b29f5784fd03b67609faf04859fe
Guilherme Conti <guibasconti@gmail.com>
2019-11-11 13:42:23 -0300

    Hackerrank Array - Bribes

---
+++ hackerrank/arrays/bribes.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    bribes = 0
+    chaotic = False
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            chaotic = True
+            break
+        for j in range(max([0, q[i] - 2]), i):
+            if q[j] > q[i]:
+                bribes += 1
+    if chaotic:
+        print ('Too chaotic')
+    else:
+        print (bribes)
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
