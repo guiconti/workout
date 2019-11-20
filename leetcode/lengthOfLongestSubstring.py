9a8a329adda75ae42512eb6b301e0c58c4bb2cac
Guilherme Conti <guibasconti@gmail.com>
2019-11-20 18:22:03 -0300

    Leetcode - Length of longest substring Python improved

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -1,3 +1,10 @@
+# Given a string, find the length of the longest substring without repeating characters.
+# Example
+# Input: "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3. 
+#              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
 class Solution:
   def lengthOfLongestSubstring(self, s: str) -> int:
     dicts = {}
