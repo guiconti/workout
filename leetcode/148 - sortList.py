ce3156dfa9552fc155d9131c02b8b407a0fe2c18
Guilherme Conti <guibasconti@gmail.com>
2020-01-14 18:19:04 -0300

    Leetcode - 148 - Sort list Python

--- leetcode/148 - sortList.py
+++ leetcode/148 - sortList.py
@@ -1,3 +1,24 @@
+# 148. Sort List
+# Medium
+
+# 2038
+
+# 102
+
+# Add to List
+
+# Share
+# Sort a linked list in O(n log n) time using constant space complexity.
+
+# Example 1:
+
+# Input: 4->2->1->3
+# Output: 1->2->3->4
+# Example 2:
+
+# Input: -1->5->3->4->0
+# Output: -1->0->3->4->5
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
