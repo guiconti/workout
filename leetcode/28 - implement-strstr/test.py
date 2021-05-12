97369a313ff25ca4a5d34e0353f0e53dc866adbd
unknown <guibasconti@gmail.com>
2021-05-11 21:24:51 -0400

    Leetcode - 28 Implement strStr

--- leetcode/28 - implement-strstr/test.py
+++ leetcode/28 - implement-strstr/test.py
@@ -8,16 +8,16 @@ unknown_answer_token = '?'
 def test():
   with open(os.path.join(os.path.dirname(__file__),  'input.txt'), 'r') as file:
     while True:
-      nums = file.readline()
-      if not nums:
+      a = file.readline()
+      if not a:
         # End of tests
         return
-      nums = ast.literal_eval(nums)
-      target = ast.literal_eval(file.readline())
+      a = ast.literal_eval(a)
+      b = ast.literal_eval(file.readline())
       answer = file.readline()
       if unknown_answer_token != answer:
         answer = ast.literal_eval(answer)
-      result = Solution().twoSum(nums, target)
+      result = Solution().strStr(a, b)
       print(f'Result given {result}')
       print(f'Correct answer {answer}')
           
