ac63397f8e820cfa5ada9096259632f24a351b39
unknown <guibasconti@gmail.com>
2021-05-13 14:12:57 -0400

    Leetcode - 2 Add Two Numbers

--- leetcode/2 - add-two-numbers/solution.py
+++ leetcode/2 - add-two-numbers/solution.py
@@ -1,31 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        answer = ListNode(0)
-        currentAnswerNode = answer
-        exceededSum = 0
-        
-        while l1 or l2 or exceededSum > 0:
-          if l1:
-            currentAnswerNode.val += l1.val
-          if l2:
-            currentAnswerNode.val += l2.val
-          currentAnswerNode.val += exceededSum
-          exceededSum = 0
-          if currentAnswerNode.val // 10 > 0:
-            exceededSum = currentAnswerNode.val // 10
-            currentAnswerNode.val = currentAnswerNode.val % 10
-          if l1:
-            l1 = l1.next
-          if l2:
-            l2 = l2.next
-          if l1 or l2 or exceededSum > 0:
-            currentAnswerNode.next = ListNode(0)
-            currentAnswerNode = currentAnswerNode.next
-        
-        return answer
+  def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    answer = ListNode(0)
+    current_answer_node = answer
+    exceeded_sum = 0
+
+    while l1 or l2 or exceeded_sum > 0:
+      if l1:
+        current_answer_node.val += l1.val
+        l1 = l1.next
+      if l2:
+        current_answer_node.val += l2.val
+        l2 = l2.next
+      current_answer_node.val += exceeded_sum
+      exceeded_sum = 0
+      if current_answer_node.val >= 10:
+        exceeded_sum = current_answer_node.val // 10
+        current_answer_node.val = current_answer_node.val % 10
+      if l1 or l2 or exceeded_sum > 0:
+        current_answer_node.next = ListNode(0)
+        current_answer_node = current_answer_node.next
+
+    return answer
--- leetcode/2 - add-two-numbers/test.py
+++ leetcode/2 - add-two-numbers/test.py
@@ -5,19 +5,46 @@ import os
 
 unknown_answer_token = '?'
 
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
 def test():
   with open(os.path.join(os.path.dirname(__file__),  'input.txt'), 'r') as file:
     while True:
-      nums = file.readline()
-      if not nums:
+      a = file.readline()
+      if not a:
         # End of tests
         return
-      nums = ast.literal_eval(nums)
-      target = ast.literal_eval(file.readline())
+      a = ast.literal_eval(a)
+      first_parameter = None
+      previous_node = None
+      for num in a:
+        new_node = ListNode(num)
+        if not first_parameter:
+          first_parameter = new_node
+        if previous_node:
+          previous_node.next = new_node
+        previous_node = new_node
+      b = ast.literal_eval(file.readline())
+      second_parameter = None
+      previous_node = None
+      for num in b:
+        new_node = ListNode(num)
+        if not second_parameter:
+          second_parameter = new_node
+        if previous_node:
+          previous_node.next = new_node
+        previous_node = new_node
       answer = file.readline()
       if unknown_answer_token != answer:
         answer = ast.literal_eval(answer)
-      result = Solution().twoSum(nums, target)
-      print(f'Result given {result}')
+      result: ListNode = Solution().addTwoNumbers(first_parameter, second_parameter)
+      result_text = []
+      while not result is None:
+        result_text.append(result.val)
+        result = result.next
+      print(f'Result given {result_text}')
       print(f'Correct answer {answer}')
           
