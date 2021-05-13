67a51c8e88e6b93d902f52b486f45a0a39b5885d
unknown <guibasconti@gmail.com>
2021-05-13 14:56:42 -0400

    Leetcode - 69 Sqrt

--- leetcode/69 - sqrtx/solution.py
+++ leetcode/69 - sqrtx/solution.py
@@ -1,3 +1,20 @@
+# Binary search
 class Solution:
-    def mySqrt(self, x: int) -> int:
-        
+  def mySqrt(self, x: int) -> int:
+    left = 0
+    # Biggest square root, which is sqrt(2^31 - 1)
+    right = 46340
+
+    while left <= right:
+      # print(left, right)
+      middle = (left + right) // 2
+      middle_powered = middle * middle
+      if middle_powered == x:
+        return middle
+      if middle_powered < x:
+        left = middle + 1
+        continue
+      right = middle - 1
+
+    return left - 1
+
