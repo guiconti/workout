6f62bfa6e94a897806a2b9efd4e8c4281e41de8c
unknown <guibasconti@gmail.com>
2021-05-14 20:27:28 -0400

    Leetcode - 66 Plus one

--- leetcode/66.plus-one/66.plus-one.py
+++ leetcode/66.plus-one/66.plus-one.py
@@ -1,3 +1,18 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        
+  def plusOne(self, digits: List[int]) -> List[int]:
+    exceededSum = 0
+    for i in range(len(digits) - 1, -1, -1):
+      if exceededSum > 0:
+        digits[i] += exceededSum
+        exceededSum = 0
+      else:
+        digits[i] += 1
+
+      if digits[i] >= 10:
+        exceededSum = digits[i] // 10
+        digits[i] = digits[i] % 10
+        continue
+      return digits
+    if exceededSum > 0:
+      return [exceededSum] + digits
+    return digits
