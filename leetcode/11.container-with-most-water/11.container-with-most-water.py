1d6fe4b17299ebddd47a134437548f16ef2e15b2
unknown <guibasconti@gmail.com>
2021-05-16 03:21:01 -0400

    Leetcode - 11 Container with most water

--- leetcode/11.container-with-most-water/11.container-with-most-water.py
+++ leetcode/11.container-with-most-water/11.container-with-most-water.py
@@ -1,3 +1,16 @@
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        
+	def maxArea(self, heights: List[int]) -> int:
+		maxArea = 0
+		i = 0
+		j = len(heights) - 1
+
+		while i < j:
+			height = min(heights[i], heights[j])
+			width = j - i
+			area = height * width
+			maxArea = max(maxArea, area)
+			if heights[i] < heights[j]:
+				i += 1
+			else:
+				j -= 1
+		return maxArea
