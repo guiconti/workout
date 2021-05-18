122e34bcd4c8c6db89c0f57ac869f907aaae611d
unknown <guibasconti@gmail.com>
2021-05-18 02:56:45 -0400

    Leetcode - 104 Maximum depth of binary tree

--- leetcode/104.maximum-depth-of-binary-tree/104.maximum-depth-of-binary-tree.py
+++ leetcode/104.maximum-depth-of-binary-tree/104.maximum-depth-of-binary-tree.py
@@ -5,5 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        
+  def getMaxDepth(self, node: TreeNode, currentDepth: int) -> int:
+    if not node:
+      return currentDepth
+    currentDepth += 1
+    depthLeft = self.getMaxDepth(node.left, currentDepth)
+    depthRight = self.getMaxDepth(node.right, currentDepth)
+    if depthLeft > depthRight:
+      return depthLeft
+    return depthRight
+  def maxDepth(self, root: TreeNode) -> int:
+    return self.getMaxDepth(root, 0)
