16985536c3c90011de7e56074a64f0b9d350d183
unknown <guibasconti@gmail.com>
2021-05-19 00:58:42 -0400

    Leetcode - 108 Convert Sorted Array To Binary Search Tree

--- leetcode/108.convert-sorted-array-to-binary-search-tree/108.convert-sorted-array-to-binary-search-tree.py
+++ leetcode/108.convert-sorted-array-to-binary-search-tree/108.convert-sorted-array-to-binary-search-tree.py
@@ -5,5 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
-        
+  def constructSubTree(self, nums: List[int], left: int, right: int):
+    if left > right:
+      return None
+    middle = (left + right) // 2
+    root = TreeNode(nums[middle])
+    root.left = self.constructSubTree(nums, left, middle - 1)
+    root.right = self.constructSubTree(nums, middle + 1, right)
+    return root
+  def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+    return self.constructSubTree(nums, 0, len(nums) - 1)
