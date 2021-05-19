542a561395c8a96448ea6d2b998d8d4042b2e199
unknown <guibasconti@gmail.com>
2021-05-19 00:59:01 -0400

    Leetcode - 110 Balanced Binary Tree

--- leetcode/110.balanced-binary-tree/110.balanced-binary-tree.py
+++ leetcode/110.balanced-binary-tree/110.balanced-binary-tree.py
@@ -5,5 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isBalanced(self, root: TreeNode) -> bool:
-        
+  def getDepth(self, root: TreeNode, currentDepth: int) -> int:
+    if not root:
+      return currentDepth
+    leftDepth = self.getDepth(root.left, currentDepth + 1)
+    rightDepth = self.getDepth(root.right, currentDepth + 1)
+    if leftDepth == -1 or rightDepth == -1 or abs(leftDepth - rightDepth) > 1:
+      return -1
+    return max(leftDepth, rightDepth)
+  def isBalanced(self, root: TreeNode) -> bool:
+    if not root:
+      return True
+    leftDepth = self.getDepth(root.left, 0)
+    rightDepth = self.getDepth(root.right, 0)
+    return leftDepth > -1 and rightDepth > -1 and abs(leftDepth - rightDepth) <= 1
