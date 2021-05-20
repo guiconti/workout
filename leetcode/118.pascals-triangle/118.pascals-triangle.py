8d20b25c6601c4ab32d269541b6fc6f5084e4247
unknown <guibasconti@gmail.com>
2021-05-20 00:12:32 -0400

    Leetcode - 118 Pascals Triangle

--- leetcode/118.pascals-triangle/118.pascals-triangle.py
+++ leetcode/118.pascals-triangle/118.pascals-triangle.py
@@ -1,3 +1,10 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        
+  def generate(self, numRows: int) -> List[List[int]]:
+    result = []
+    for i in range(1, numRows + 1):
+      for j in range(i):
+        if j == 0 or j == i - 1:
+          result[i - 1].append(1)
+          continue
+        result[i - 1].append(result[i - 2][j - 1] + result[i - 2][j])
+    return result
