b1517503e37f7c90c960cc96c1086f2e9370efd5
unknown <guibasconti@gmail.com>
2021-05-19 23:59:52 -0400

    Leetcode - 112 Path Sum

--- leetcode/112.path-sum/112.path-sum.py
+++ leetcode/112.path-sum/112.path-sum.py
@@ -5,5 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        
+  def A(self, root: TreeNode, currentSum: int, targetSum: int) -> bool:
+    if not root:
+      return False
+    currentSum += root.val
+    if not root.left and not root.right:
+      return currentSum == targetSum
+    if root.left and self.A(root.left, currentSum, targetSum):
+      return True
+    if root.right:
+      return self.A(root.right, currentSum, targetSum)
+    return False
+  def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+    if not root:
+      return False
+    if not root.left and not root.right:
+      return root.val == targetSum
+    return self.A(root.left, root.val, targetSum) or self.A(root.right, root.val, targetSum)
