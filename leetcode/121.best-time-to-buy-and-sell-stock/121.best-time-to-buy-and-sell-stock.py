3fd69ffd153aad18daecc2750e66556711af6afc
unknown <guibasconti@gmail.com>
2021-05-21 13:07:07 -0400

    Leetcode - 121 Best time to buy and sell stock

--- leetcode/121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.py
+++ leetcode/121.best-time-to-buy-and-sell-stock/121.best-time-to-buy-and-sell-stock.py
@@ -1,3 +1,11 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
+  def maxProfit(self, prices: List[int]) -> int:
+    minPrice = 1000000000
+    maxProfit = 0
+    for price in prices:
+      if price < minPrice:
+        minPrice = price
+        continue
+      if (price - minPrice > maxProfit):
+        maxProfit = price - minPrice
+    return maxProfit
