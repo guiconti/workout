343c803972b31d936f32a11d1d3d0f4ef3be697d
unknown <guibasconti@gmail.com>
2021-05-21 13:07:32 -0400

    Leetcode - 125 Valid palindrome

--- leetcode/125.valid-palindrome/125.valid-palindrome.py
+++ leetcode/125.valid-palindrome/125.valid-palindrome.py
@@ -1,3 +1,5 @@
+import re
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        
+  def isPalindrome(self, s: str) -> bool:
+    s = re.sub(r'\W|_+', '', s).lower()
+    return s == s[::-1]
