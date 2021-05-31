9642ac483639bd77643cd9701950deb791cee77e
unknown <guibasconti@gmail.com>
2021-05-31 03:12:14 -0400

    Leetcode - 172 Factorial Trailing Zeroes

--- leetcode/172.factorial-trailing-zeroes/172.factorial-trailing-zeroes.py
+++ leetcode/172.factorial-trailing-zeroes/172.factorial-trailing-zeroes.py
@@ -1,3 +1,15 @@
 class Solution:
-    def trailingZeroes(self, n: int) -> int:
-        
+  def trailingZeroes(self, n: int) -> int:
+    if n <= 1:
+      return 0
+    factorial = 1
+    for i in range(2, n + 1):
+      factorial *= i
+    factorial = str(factorial)[::-1]
+    result = 0
+    for character in factorial:
+      if character == '0':
+        result += 1
+        continue
+      return result
+    return result
