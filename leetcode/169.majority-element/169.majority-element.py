ce746deeeec08ce1d1d8d9ab75588f7613434a07
unknown <guibasconti@gmail.com>
2021-05-31 03:06:54 -0400

    Leetcode - 169 Majority Element

--- leetcode/169.majority-element/169.majority-element.py
+++ leetcode/169.majority-element/169.majority-element.py
@@ -1,3 +1,5 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+  def majorityElement(self, nums: List[int]) -> int:
+    nums.sort()
+    return nums[len(nums) // 2]
         
