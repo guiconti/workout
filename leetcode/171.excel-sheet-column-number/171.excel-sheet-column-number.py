18fcf610e8a856010bed79cce9bbfa9150bb7191
unknown <guibasconti@gmail.com>
2021-05-31 11:36:45 -0400

    Leetcode - 171 Excel sheet column number

--- leetcode/171.excel-sheet-column-number/171.excel-sheet-column-number.py
+++ leetcode/171.excel-sheet-column-number/171.excel-sheet-column-number.py
@@ -1,3 +1,7 @@
 class Solution:
-    def titleToNumber(self, columnTitle: str) -> int:
-        
+  def titleToNumber(self, columnTitle: str) -> int: 
+    result = 0
+    columnTitle = columnTitle[::-1]
+    for i in range(0, len(columnTitle)):
+        result += (ord(columnTitle[i]) - 64) * (26 ** i)
+    return result
