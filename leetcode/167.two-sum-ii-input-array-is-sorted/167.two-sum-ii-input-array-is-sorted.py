fa0e60606da882d3e352c4d55d23b7707827dea3
unknown <guibasconti@gmail.com>
2021-05-31 02:54:26 -0400

    Leetcode - 167 Two Sum II Input Array Is Sorted

--- leetcode/167.two-sum-ii-input-array-is-sorted/167.two-sum-ii-input-array-is-sorted.py
+++ leetcode/167.two-sum-ii-input-array-is-sorted/167.two-sum-ii-input-array-is-sorted.py
@@ -1,3 +1,10 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+  def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    hashMap = {}
+    for i in range(len(numbers)):
+      hashMap[numbers[i]] = i 
+    for i in range(len(numbers)):
+      matchingNumber = target - numbers[i]
+      if matchingNumber in hashMap:
+        return [i + 1, hashMap[matchingNumber] + 1]
+    return [-1, -1]
