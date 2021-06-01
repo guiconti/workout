50351513e49174ac035db95eccf83615eb5281fe
unknown <guibasconti@gmail.com>
2021-06-01 02:20:24 -0400

    Leetcode - 567 Permutation in string

--- leetcode/567.permutation-in-string/567.permutation-in-string.py
+++ leetcode/567.permutation-in-string/567.permutation-in-string.py
@@ -1,3 +1,16 @@
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        
+  def checkInclusion(self, s1: str, s2: str) -> bool:
+    s1 = sorted(s1)
+    candidate = ''
+    for i in range(len(s2) - len(s1) + 1):
+      if i == 0:
+        candidate = sorted(s2[i:i + len(s1)])
+      else:
+        candidate = candidate[1:]
+
+      if s1 == candidate:
+        return True
+    return False
+
+    # Theres a better way where we store the occurences in a 26 sized array
+    # Then duplicate the array on every try and see if it matches.
