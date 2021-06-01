e6e6159a1f462ebf50a18aad881a7fb75863b73b
unknown <guibasconti@gmail.com>
2021-06-01 02:45:55 -0400

    Leetcode - 326 Power Of Three

--- leetcode/326.power-of-three/326.power-of-three.py
+++ leetcode/326.power-of-three/326.power-of-three.py
@@ -1,3 +1,12 @@
 class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        
+  def isPowerOfThree(self, n: int) -> bool:
+    if n == 0:
+      return False
+    elif n == 1:
+      return True
+    while (n != 1):
+      n = (float(n)/3)
+      if n < 1:
+        return False
+      elif n == 1:
+        return True
