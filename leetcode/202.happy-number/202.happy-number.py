b0e4ea435f9e58ed4e5244b5b63b0eb46fc80580
unknown <guibasconti@gmail.com>
2021-06-08 02:33:10 -0400

    Leetcode - 202 Happy Number

--- leetcode/202.happy-number/202.happy-number.py
+++ leetcode/202.happy-number/202.happy-number.py
@@ -1,3 +1,13 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        
+  def isHappy(self, n: int) -> bool:
+    cache = set()
+
+    while n != 1:
+      if n in cache:
+        return False
+      cache.add(n)
+      array = [int(digit) for digit in str(n)]
+      n = 0
+      for digit in array:
+        n += digit * digit
+    return True
