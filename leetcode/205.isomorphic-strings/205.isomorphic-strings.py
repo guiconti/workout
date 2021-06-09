a666bf39e59e9351e116febb977d909333165290
unknown <guibasconti@gmail.com>
2021-06-08 22:25:38 -0400

    Leetcode - 205 Isomorphic strings

--- leetcode/205.isomorphic-strings/205.isomorphic-strings.py
+++ leetcode/205.isomorphic-strings/205.isomorphic-strings.py
@@ -1,3 +1,17 @@
 class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        
+  def isIsomorphic(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+    hashMap = {}
+    cache = set()
+    for i in range(len(s)):
+      if t[i] in hashMap:
+        if hashMap[t[i]] != s[i]:
+          return False
+        continue
+      if s[i] in cache:
+        return False
+      cache.add(s[i])
+      hashMap[t[i]] = s[i]
+
+    return True
