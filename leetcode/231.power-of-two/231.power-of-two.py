a3311a273376a03302d83a571bc71f3cb97a4870
unknown <guibasconti@gmail.com>
2021-06-16 01:47:21 -0400

    Leetcode - 228 Summary Ranges

--- leetcode/231.power-of-two/231.power-of-two.py
+++ leetcode/231.power-of-two/231.power-of-two.py
@@ -1,3 +1,9 @@
 class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        
+  def isPowerOfTwo(self, n: int) -> bool:
+    for i in range(n + 1):
+      current = 2 ** i
+      if current == n:
+        return True
+      if current > n:
+        return False
+    return False
