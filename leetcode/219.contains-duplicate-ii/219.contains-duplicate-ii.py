05abc6bd839b24d9557a8258dea4ed8b6dd407c2
unknown <guibasconti@gmail.com>
2021-06-18 11:21:23 -0400

    Leetcode - 2199 Contains duplicate

--- leetcode/219.contains-duplicate-ii/219.contains-duplicate-ii.py
+++ leetcode/219.contains-duplicate-ii/219.contains-duplicate-ii.py
@@ -1,3 +1,8 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        
+  def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    hashMap = {}
+    for i in range(len(nums)):
+      if nums[i] in hashMap and abs(hashMap[nums[i]] - i) <= k:
+          return True
+      hashMap[nums[i]] = i
+    return False
