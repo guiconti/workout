18beaeffa4d0ba75f8dcab5e3165a964d9fd8433
unknown <guibasconti@gmail.com>
2021-06-23 02:29:00 -0400

    Leetcode - 257 Binary Tree Paths

--- leetcode/257.binary-tree-paths/257.binary-tree-paths.py
+++ leetcode/257.binary-tree-paths/257.binary-tree-paths.py
@@ -5,5 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        
+  def getPaths(self, node: TreeNode, currentPath: str, paths: List[str]):
+    if node == None:
+      return
+    currentPath += f"->{node.val}"
+    if not node.left and not node.right:
+      paths.append(currentPath)
+      return
+    self.getPaths(node.left, currentPath, paths)
+    self.getPaths(node.right, currentPath, paths)
+
+  def binaryTreePaths(self, root: TreeNode) -> List[str]:
+    if root == None:
+      return []
+    currentPath = str(root.val)
+    if not root.left and not root.right:
+      return [currentPath]
+    paths = []
+    self.getPaths(root.left, currentPath, paths)
+    self.getPaths(root.right, currentPath, paths)
+    return paths
