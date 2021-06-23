ab9e2bd3da17187f2eed6425d8bb0ee1fe7c3317
unknown <guibasconti@gmail.com>
2021-06-23 02:10:20 -0400

    Leetcode - 242 Valid Anagram

--- leetcode/242.valid-anagram/242.valid-anagram.py
+++ leetcode/242.valid-anagram/242.valid-anagram.py
@@ -1,3 +1,19 @@
+from typing import Hashable
+
+
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+    hashMap = {}
+    for character in s:
+      if not character in hashMap:
+        hashMap[character] = 0
+      hashMap[character] += 1
+    for character in t:
+      if not character in hashMap:
+        return False
+      if hashMap[character] <= 0:
+        return False
+      hashMap[character] -= 1
+    return True
