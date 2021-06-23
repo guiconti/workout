995ec916dc6742091a84cff160356e49bd32f5dc
unknown <guibasconti@gmail.com>
2021-06-23 02:31:47 -0400

    Leetcode - 258 Add Digits

--- leetcode/258.add-digits/258.add-digits.py
+++ leetcode/258.add-digits/258.add-digits.py
@@ -1,3 +1,8 @@
 class Solution:
-    def addDigits(self, num: int) -> int:
-        
+  def addDigits(self, num: int) -> int:
+    while num / 10 >= 1:
+      strNum = str(num)
+      num = 0
+      for character in strNum:
+        num += int(character)
+    return num
