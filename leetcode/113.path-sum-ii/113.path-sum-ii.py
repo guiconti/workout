3b200845a6672342cada7df953d6015fd876c269
unknown <guibasconti@gmail.com>
2021-06-23 02:45:08 -0400

    Leetcode - 113 Path Sum II

--- leetcode/113.path-sum-ii/113.path-sum-ii.py
+++ leetcode/113.path-sum-ii/113.path-sum-ii.py
@@ -5,5 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
-        
+  def fillPaths(self, node: TreeNode, targetSum: int, currentSum: int, currentPath: str, result: List[List[int]]):
+    if node == None:
+      return
+    currentSum += node.val
+    currentPath += f",{node.val}"
+    if node.left == None and node.right == None:
+      if currentSum == targetSum:
+        result.append(list(currentPath.split(",")))
+      return
+    self.fillPaths(node.left, targetSum, currentSum, currentPath, result)
+    self.fillPaths(node.right, targetSum, currentSum, currentPath, result)
+  def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+    result = []
+    if root == None:
+      return result
+    if root.left == None and root.right == None:
+      if root.val == targetSum:
+        result.append([root.val])
+      return result
+    self.fillPaths(root.left, targetSum, root.val, str(root.val), result)  
+    self.fillPaths(root.right, targetSum, root.val, str(root.val), result)  
+    return result
