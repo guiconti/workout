a2ba93fbb4c259fed68be1294bbb5f77c28a1937
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-06-24 00:33:42 -0400

    Leetcode - 290 Word Pattern

--- leetcode/290.word-pattern/290.word-pattern.py
+++ leetcode/290.word-pattern/290.word-pattern.py
@@ -1,3 +1,16 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        
+  def wordPattern(self, pattern: str, s: str) -> bool:
+    words = s.split(' ')
+    if len(words) != len(pattern):
+      return
+    hashMap = {}
+    patternSet = set()
+    for i in range(len(words)):
+      if words[i] in hashMap:
+        if hashMap[words[i]] != pattern[i]:
+          return False
+      elif pattern[i] in patternSet:
+        return False 
+      hashMap[words[i]] = pattern[i]
+      patternSet.add(pattern[i])
+    return True
