ae481cb7e98475e922b5ae8d7ee554601d2229c8
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-01 01:06:49 -0400

    Leetcode - 235 Lowest common ancestor of a binary search tree

--- leetcode/235.lowest-common-ancestor-of-a-binary-search-tree/235.lowest-common-ancestor-of-a-binary-search-tree.py
+++ leetcode/235.lowest-common-ancestor-of-a-binary-search-tree/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,5 +6,22 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+  def isNodeUnderRoot(self, root, value):
+    if not root:
+      return False
+    if root.val == value:
+      return True
+    return self.isNodeUnderRoot(root.left, value) or self.isNodeUnderRoot(root.right, value)
+
+  def findAncestor(self, root, p: TreeNode, q: TreeNode) -> TreeNode:
+    if not root or root.val == p.val or root.val == q.val:
+      return root
+    isPOnLeft = self.isNodeUnderRoot(root.left, p.val)
+    isQOnLeft = self.isNodeUnderRoot(root.left, q.val)
+    if isPOnLeft != isQOnLeft:
+      return root
+    newRoot = root.left if isPOnLeft else root.right
+    return self.findAncestor(newRoot, p, q)
+
+  def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    return self.findAncestor(root, p, q)
