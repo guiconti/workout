687781a2bfff1a52e8f2abf3186cd46653f5a5d8
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-06 01:44:29 -0400

    Leetcode - 48 Rotate Image

--- leetcode/48.rotate-image/48.rotate-image.py
+++ leetcode/48.rotate-image/48.rotate-image.py
@@ -1,6 +1,22 @@
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+  def rotate(self, matrix: List[List[int]]) -> None:
+    if len(matrix) == 0 or len(matrix) != len(matrix[0]):
+      return
+
+    for i in range(len(matrix) // 2):
+      first = i
+      last = len(matrix) - 1 - i
+      for j in range(first, last):
+        # Essentially the column
+        offset = j - first
         
+        # Save top
+        top = matrix[first][j]
+        # Left to top
+        matrix[first][j] = matrix[last - offset][first]
+        # Bottom to left
+        matrix[last - offset][first] = matrix[last][last - offset]
+        # Right to Bottom
+        matrix[last][last - offset] = matrix[j][last]
+        # Top to right
+        matrix[j][last] = top
