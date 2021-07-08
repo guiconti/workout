fa44adcb1f96619e50651f0e60ed8092f4f9a4d4
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-08 03:19:50 -0400

    Leetcode - 234 Palindrome Linked List

--- leetcode/234.palindrome-linked-list/234.palindrome-linked-list.py
+++ leetcode/234.palindrome-linked-list/234.palindrome-linked-list.py
@@ -4,5 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
+  def isPalindrome(self, head: ListNode) -> bool:
+    slow, fast = head, head
+    stack = []
+    
+    while fast != None and fast.next != None:
+        stack.append(slow.val)
+        fast = fast.next.next
+        slow = slow.next
+
+    #     Odd length
+    if fast != None:
+        slow = slow.next
         
+    while slow != None:
+        if slow.val != stack.pop():
+            return False
+        slow = slow.next
+    return True
