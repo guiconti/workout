48f62f4f35464f57f8d09233560e00026fb068f8
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-08 02:00:26 -0400

    Leetcode - 19 Remove nth node from end of list

--- leetcode/19.remove-nth-node-from-end-of-list/19.remove-nth-node-from-end-of-list.py
+++ leetcode/19.remove-nth-node-from-end-of-list/19.remove-nth-node-from-end-of-list.py
@@ -4,5 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+  def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+    slowNode = head
+    fastNode = head
+
+    i = 0
+    while i < n and fastNode != None:
+      fastNode = fastNode.next
+      i += 1
+    
+    if fastNode == None:
+      head = head.next
+      return head
+
+    while fastNode.next != None:
+      slowNode = slowNode.next
+      fastNode = fastNode.next
         
+    slowNode.next = slowNode.next.next
+    
+    return head
