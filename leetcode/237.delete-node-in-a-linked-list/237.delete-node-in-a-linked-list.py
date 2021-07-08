f85bbd43fc30f2fd05df4362260d3f98b6abe852
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-08 02:10:48 -0400

    Leetcode - 237 Delete node in a linked list

--- leetcode/237.delete-node-in-a-linked-list/237.delete-node-in-a-linked-list.py
+++ leetcode/237.delete-node-in-a-linked-list/237.delete-node-in-a-linked-list.py
@@ -5,9 +5,13 @@
 #         self.next = None
 
 class Solution:
-    def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
+  def deleteNode(self, node):
+    previous = node
+    node = node.next
+    while node.next != None:
+        previous.val = node.val
+        node = node.next
+        previous = previous.next
+    previous.val = node.val
+    previous.next = None
         
