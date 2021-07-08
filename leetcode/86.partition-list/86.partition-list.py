f60933d04613585b41069535cbed18b07ffd252d
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-08 02:57:49 -0400

    Leetcode - 89 Partition list

--- leetcode/86.partition-list/86.partition-list.py
+++ leetcode/86.partition-list/86.partition-list.py
@@ -5,4 +5,38 @@
 #         self.next = next
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
-        
+        rightHead = None
+        currentRight = None
+        current = head
+        leftTail = head
+
+        while current != None:
+            if current.val < x:
+                leftTail = current
+                current = current.next
+                continue
+            
+            movedHead = False
+            if current == head:
+                if current.next == None and rightHead == None:
+                    return head
+                head = current.next
+                leftTail = head
+                movedHead = True
+
+            if rightHead == None:
+                rightHead = current
+                currentRight = rightHead
+            else:
+                currentRight.next = current
+                currentRight = current
+            if not movedHead:
+                leftTail.next = current.next
+            current = current.next
+
+        if leftTail == None:
+            return rightHead          
+        leftTail.next = rightHead
+        if currentRight != None:
+            currentRight.next = None
+        return head
