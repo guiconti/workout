00856ee4ac1e69f62d1566516c7f15f128afcff8
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-08 02:00:53 -0400

    CTCI - 1-7 Rotate matrix

--- crackingthecodeinterview/arrays/1-7.py
+++ crackingthecodeinterview/arrays/1-7.py
@@ -1,9 +1,10 @@
 # Given an image represented by NXN matrix where each pixel is 4 bytes
 # Write a method to rotate it in 90 degress. Can you do it in place
 
-# Solution 1 Iterate through the string and while the next is equals to the current
-# increment your current sum, when you found a different string append your result
-# and restart your count O(n)
+# Solution 1 The easiest way to do this is to implement the rotation in layers.
+# We perform a circular rotation on each layer, moving the top edge to the right edge, the right edge 
+# to the bottom edge, the bottom edge to the left edge, and the left edge to the top edge
+# We perform such a swap on each layer, starting from the outermost layer and working our way inwards.
 def Solution1(test):
   if len(test) == 0 or len(test) != len(test[0]):
     return False
@@ -14,12 +15,18 @@ def Solution1(test):
     first = i
     last = n - 1 - i
     for j in range(first, last):
+      # Essentially the column
       offset = j - first
       
+      # Save top
       top = test[first][j]
+      # Left to top
       test[first][j] = test[last - offset][first]
+      # Bottom to left
       test[last - offset][first] = test[last][last - offset]
+      # Right to Bottom
       test[last][last - offset] = test[j][last]
+      # Top to right
       test[j][last] = top
   
   return test
